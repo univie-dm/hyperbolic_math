2877c4f99606442464cfd8fee67fce7680142d67
Timo Klein <tkleia34@gmail.com>
2024-11-27 14:49:29 +0100

    fix embedding c type and add other typing information

--- src/manifolds/embedding.py
+++ src/manifolds/embedding.py
@@ -1,6 +1,6 @@
 import torch
 
-from .manifold import ManifoldParameter
+from .manifold import ManifoldParameter, Manifold
 
 
 class Embedding(torch.nn.Module):
@@ -8,12 +8,15 @@ class Embedding(torch.nn.Module):
     Embedding layer that supports different manifolds.
     """
 
-    # TODO: Add type information here for better autocomplete information
+    manifold: Manifold
+    c: torch.Tensor
+    weight: ManifoldParameter
+    bias: ManifoldParameter
 
     def __init__(self, input_dim: int, output_dim: int, manifold: str, c: float, requires_grad: bool = True):
         super(Embedding, self).__init__()
         self.manifold = manifold
-        self.c = c  # Curvature for non-Euclidean manifolds
+        self.c = torch.tensor(c, dtype=torch.float32)  # Curvature for non-Euclidean manifolds
 
         weight = torch.randn(input_dim, output_dim)
         self.weight = ManifoldParameter(weight, requires_grad=requires_grad, manifold=self.manifold, c=self.c)
@@ -23,7 +26,7 @@ class Embedding(torch.nn.Module):
 
     def forward(self, x):
         # We always need a bias addition, else it's just equivalent to a Euclidean NN
-        #TODO: Alternative bias translation: expmap(ptransp_0(bias, x), x, c) -- Stability??
+        # TODO: Alternative bias translation: expmap(ptransp_0(bias, x), x, c) -- Stability??
 
         # Method 1: matvec_mul == expmap_0(W*logmap_0(x))
         # x = self.manifold.expmap_0(x, self.c)
@@ -33,5 +36,5 @@ class Embedding(torch.nn.Module):
         # Method 2 - less computation since expmap_0(logmap_0(x)==x), hence theoretically more stable - test in experiments:
         x = self.manifold.expmap_0(x @ self.weight, self.c)
         x = self.manifold.addition(x, self.bias, self.c)
-        
+
         return x
