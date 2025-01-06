correct bias shapes for mat-vec mult.; implement correct affine layer computation via hyperplane distance as a new method

src/manifolds/embedding.py
@@ -6,7 +6,7 @@ from .manifold import Manifold, ManifoldParameter
 from .poincare import PoincareBall
 
 ForwardPassType = Literal[
-    "dist2hyperplane", "dist2hyperplane_pp", "direct_matvec_mul", "indirect_matvec_mul", "hnn_matvec_mul"
+    "dist2hyperplane", "dist2hyperplane_correct", "dist2hyperplane_pp", "direct_matvec_mul", "indirect_matvec_mul", "hnn_matvec_mul"
 ]
 
 
@@ -41,51 +41,104 @@ class Embedding(torch.nn.Module):
         weight = torch.randn(input_dim, output_dim)
         self.weight = torch.nn.Parameter(weight, requires_grad=requires_grad)
 
-        bias = torch.zeros(input_dim)
+        if forward_method in {"dist2hyperplane", "dist2hyperplane_correct", "dist2hyperplane_pp"}:
+            bias = torch.zeros(input_dim)
+        elif forward_method in {"direct_matvec_mul", "indirect_matvec_mul", "hnn_matvec_mul"}:
+            bias = torch.zeros(output_dim)
         self.bias = ManifoldParameter(bias, requires_grad=requires_grad, manifold=self.manifold, c=self.c)
 
     def _sanity_checks(self, forward_method: ForwardPassType) -> None:
         """Sanity checks to ensure correct initialization and forward method"""
 
         assert forward_method in get_args(ForwardPassType), f"Invalid forward method: {forward_method}"
-        if forward_method in {"dist2hyperplane", "dist2hyperplane++"}:
+        if forward_method in {"dist2hyperplane", "dist2hyperplane_correct", "dist2hyperplane++"}:
             assert isinstance(self.manifold, PoincareBall), "dist2hyperplane methods only work for PoincareBall manifold"
+        if forward_method == "dist2hyperplane_correct":
+            assert False, "Not implemented yet"
 
     def forward_dist2hyperplane(self, x: torch.Tensor) -> torch.Tensor:
-        """Forward pass via dist2hyperplane - only works for PoincareBall"""
+        """
+        Forward pass via dist2hyperplane:
+           1) Perform Matrix-Vector multiplication in the tangent space of the bias.
+           2) Represent the full affine function from the viewpoint of the hyperplane mx + b = 0.
+        [Only works for the PoincareBall]
+        """
+        assert self.manifold.is_in_manifold(self.bias, self.c)
+
         x = self.manifold.expmap_0(x, self.c)
         res = self.manifold.dist2hyperplane(x, self.weight, self.bias, self.c, signed=True, scaled=True)
         return res
 
+    def forward_dist2hyperplane_correct(self, x: torch.Tensor) -> torch.Tensor:
+        """
+        Forward pass via forward_dist2hyperplane_correct:
+           1) Parallel transport the weight to the tangent space of the bias.
+           2) Perform Matrix-Vector multiplication in the tangent space of the bias.
+           3) Represent the full affine function from the viewpoint of the hyperplane mx + b = 0.
+        [Only works for the PoincareBall]
+        """
+        assert self.manifold.is_in_manifold(self.bias, self.c)
+
+        tangent_space_weight = self.manifold.ptransp_0(self.weight, self.bias, self.c)
+        x = self.manifold.expmap_0(x, self.c)
+        res = self.manifold.dist2hyperplane_correct(x, tangent_space_weight, self.bias, self.c)
+        return res
+
     def forward_dist2hyperplane_pp(self, x: torch.Tensor) -> torch.Tensor:
-        """Forward pass via dist2hyperplane++ - only works for PoincareBall"""
+        """
+        #TODO
+        Forward pass via dist2hyperplane++:
+           1) Perform Matrix-Vector multiplication in the tangent space and map back to the manifold.
+           2) Add the manifold bias to the previous result.
+        [Only works for the PoincareBall]
+        """
+        assert False, "Not implemented yet"
+
+        assert self.manifold.is_in_manifold(self.bias, self.c)
+
         x = self.manifold.expmap_0(x, self.c)
         res = self.manifold.dist2hyperplane_pp(x, self.weight, self.bias, self.c, signed=True, scaled=True)
         return res
 
     def forward_direct_matvec_mul(self, x: torch.Tensor) -> torch.Tensor:
-        """Forward pass via tangent space matrix-vector mult. - more stable than "indirect_matvec_mul" """
+        """
+        Forward pass via tangent space matrix-vector mult. without logmap/expmap transforms:
+           1) Perform Matrix-Vector multiplication in the tangent space and map back to the manifold.
+           2) Add the manifold bias to the previous result.
+        """
+        assert self.manifold.is_in_manifold(self.bias, self.c)
+
         x = self.manifold.expmap_0(x @ self.weight, self.c)
         res = self.manifold.addition(x, self.bias, self.c)
         return res
 
     def forward_indirect_matvec_mul(self, x: torch.Tensor) -> torch.Tensor:
-        """Forward pass via tangent space matrix-vector mult. but with logmap/expmap transforms"""
+        """
+        Forward pass via tangent space matrix-vector mult. but with logmap/expmap transforms:
+           1) Map input x from the tangent space to the manifold.
+           2) Perform Matrix-Vector multiplication in the tangent space and map back to the manifold.
+           3) Add the manifold bias to the previous result.
+        """
+        assert self.manifold.is_in_manifold(self.bias, self.c)
+
         x = self.manifold.expmap_0(x, self.c)
         x = self.manifold.matvec_mul(self.weight, x, self.c)
         res = self.manifold.addition(x, self.bias, self.c)
         return res
 
     def forward_hnn_matvec_mul(self, x: torch.Tensor) -> torch.Tensor:
-        """Forward pass as in hyperbolic neural networks"""
+        """
+        Forward pass as in hyperbolic neural networks paper:
+           1) Perform Matrix-Vector multiplication in the tangent space and map back to the manifold.
+           2) Add the manifold bias to the previous result via parallel transport.
+           3) Map the result back to the manifold.
+        """
+        assert self.manifold.is_in_manifold(self.bias, self.c)
+
         x = self.manifold.expmap_0(x @ self.weight, self.c)
-        x = self.manifold.matvec_mul(self.weight, x, self.c)
-        ebias = self.manifold.ptransp_0(self.manifold.logmap_0(self.bias, self.c), x, self.c)
-        res = self.manifold.expmap(ebias, x, self.c)
+        bias = self.manifold.ptransp_0(self.manifold.logmap_0(self.bias, self.c), x, self.c)
+        res = self.manifold.expmap(bias, x, self.c)
         return res
 
     def forward(self, x: torch.Tensor) -> torch.Tensor:
-        # Check if bias is in manifold
-        assert self.manifold.is_in_manifold(self.bias, self.c)
-
         return self.forward_method(x)

src/manifolds/poincare.py
@@ -204,7 +204,7 @@ class PoincareBall(Manifold):
         Returns
         -------
         res : torch.Tensor
-            The product(s) of m and x
+            The product(s) of m and x defined as expmap_0(m * logmap_0(x))
 
         References
         ----------
@@ -247,24 +247,7 @@ class PoincareBall(Manifold):
     def dist2hyperplane(self, x: torch.Tensor, m: torch.Tensor, p: torch.Tensor, c: torch.Tensor,
                         signed: bool = False, scaled: bool = False) -> torch.Tensor:
         """
-        Compute the geodesic distance(s) of PoincareBall point(s) x from/to the PoincareBall origin.
-
-        Parameters
-        ----------
-        x : torch.Tensor
-            PoincareBall point(s)
-        c : torch.Tensor
-            magnitude of sectional curvature
-
-        Returns
-        -------
-        res : torch.Tensor
-            The geodesic distance(s) of x from/to the PoincareBall origin
-
-        References
-        ----------
-        Ganea, Octavian, Gary Bécigneul, and Thomas Hofmann. "Hyperbolic neural networks."
-            Advances in neural information processing systems 31 (2018).
+        #TODO
         """
         sqrt_c = c.sqrt()
         m_norm = m.norm(p=2, dim=0, keepdim=True).clamp_min(self.min_enorm)
@@ -278,6 +261,25 @@ class PoincareBall(Manifold):
         res = arsinh(num / denom) / sqrt_c
         if scaled:
             res = res * m_norm
+        return res
+    
+    def dist2hyperplane_correct(self, x: torch.Tensor, m: torch.Tensor, p: torch.Tensor, c: torch.Tensor) -> torch.Tensor:
+        """
+        #TODO
+        """
+        # Perform matrix-vector multiplication in the tangent space
+        # The row vectors of m are the hyperplane normals
+        sub = self.addition(-p, x, c)
+        msub = sub @ m
+        # Determine which side of the hyperplanes the point(s) on
+        orientation = torch.sign(msub)
+        # Get the lengths of the hyperplane normals
+        m_norm = self.tangent_norm(m.T, p, c).T
+        # Compute the geodesic distance(s) of the point(s) to the hyperplane
+        sqrt_c = c.sqrt()
+        denom = m.norm(p=2, dim=0, keepdim=True).clamp_min(self.min_enorm)
+        dist2hyp = arsinh(self._lambda(sub, c) * sqrt_c * msub.abs() / denom) / sqrt_c
+        res = orientation * dist2hyp * m_norm
         return res
 
     # def dist2hyperplane_pp(self, x: torch.Tensor, m: torch.Tensor, p: torch.Tensor, c: torch.Tensor,