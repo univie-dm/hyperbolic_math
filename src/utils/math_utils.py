d45fa9bd50170ee42213cd29051ec0dec5e76b46
Kevin Sidak <kevin.sidak@univie.ac.at>
2025-01-24 15:30:27 +0100

    Refactor hyperbolic functions to use a dedicated epsilon retrieval function

--- src/utils/math_utils.py
+++ src/utils/math_utils.py
@@ -6,25 +6,25 @@ import torch
 
 def cosh(x: torch.Tensor) -> torch.Tensor:
     """Hyperbolic cosine. Domain=(-inf, inf)."""
-    eps = torch.finfo(x.dtype).eps
+    eps = _get_tensor_eps(x)
     clamp = float(math.log(2 / eps))
     return x.clamp(-clamp, clamp).cosh()
 
 def sinh(x: torch.Tensor) -> torch.Tensor:
     """Hyperbolic sine. Domain=(-inf, inf)."""
-    eps = torch.finfo(x.dtype).eps
+    eps = _get_tensor_eps(x)
     clamp = float(math.log(2 / eps))
     return x.clamp(-clamp, clamp).sinh()
 
 def tanh(x: torch.Tensor) -> torch.Tensor:
     """Hyperbolic tangent. Domain=(-inf, inf)."""
-    eps = torch.finfo(x.dtype).eps
+    eps = _get_tensor_eps(x)
     clamp = float(-math.log(eps / 2) / 2)
     return x.clamp(-clamp, clamp).tanh()
 
 def arcosh(x: torch.Tensor) -> torch.Tensor:
     """Inverse hyperbolic cosine. Domain=[1, inf)."""
-    min_val = 1.0 + torch.finfo(x.dtype).eps
+    min_val = 1.0 + _get_tensor_eps(x)
     return x.clamp(min=min_val).double().add(torch.sqrt(x.clamp(min=min_val).double().pow(2) - 1)).log().to(x.dtype)
 
 def arsinh(x: torch.Tensor) -> torch.Tensor:
@@ -33,6 +33,22 @@ def arsinh(x: torch.Tensor) -> torch.Tensor:
 
 def artanh(x: torch.Tensor) -> torch.Tensor:
     """Inverse hyperbolic tangent. Domain=(-1, 1)."""
-    eps = torch.finfo(x.dtype).eps
+    eps = _get_tensor_eps(x)
     x = x.clamp(-1 + eps, 1 - eps)
     return (torch.log1p(x.double()) - torch.log1p(-x.double())).mul(0.5).to(x.dtype)
+
+
+def _get_tensor_eps(
+    x: torch.Tensor,
+    eps16: float = torch.finfo(torch.float16).eps,
+    eps32: float = torch.finfo(torch.float32).eps,
+    eps64: float = torch.finfo(torch.float64).eps,
+) -> float:
+    if x.dtype == torch.float16:
+        return eps16
+    elif x.dtype == torch.float32:
+        return eps32
+    elif x.dtype == torch.float64:
+        return eps64
+    else:
+        raise RuntimeError(f"Expected x to be floating-point, got {x.dtype}")
