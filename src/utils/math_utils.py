0ef30fc1a334a6797f3b702471974c8a6df214a7
94langth <thomas.lang@univie.ac.at>
2025-01-24 13:24:24 +0000

    add typing for math_utils

--- src/utils/math_utils.py
+++ src/utils/math_utils.py
@@ -4,34 +4,34 @@ import math
 import torch
 
 
-def cosh(x):
+def cosh(x: torch.Tensor) -> torch.Tensor:
     """Hyperbolic cosine. Domain=(-inf, inf)."""
     eps = torch.finfo(x.dtype).eps
     clamp = float(math.log(2 / eps))
     return x.clamp(-clamp, clamp).cosh()
 
-def sinh(x):
+def sinh(x: torch.Tensor) -> torch.Tensor:
     """Hyperbolic sine. Domain=(-inf, inf)."""
     eps = torch.finfo(x.dtype).eps
     clamp = float(math.log(2 / eps))
     return x.clamp(-clamp, clamp).sinh()
 
-def tanh(x):
+def tanh(x: torch.Tensor) -> torch.Tensor:
     """Hyperbolic tangent. Domain=(-inf, inf)."""
     eps = torch.finfo(x.dtype).eps
     clamp = float(-math.log(eps / 2) / 2)
     return x.clamp(-clamp, clamp).tanh()
 
-def arcosh(x):
+def arcosh(x: torch.Tensor) -> torch.Tensor:
     """Inverse hyperbolic cosine. Domain=[1, inf)."""
     min_val = 1.0 + torch.finfo(x.dtype).eps
     return x.clamp(min=min_val).double().add(torch.sqrt(x.clamp(min=min_val).double().pow(2) - 1)).log().to(x.dtype)
 
-def arsinh(x):
+def arsinh(x: torch.Tensor) -> torch.Tensor:
     """Inverse hyperbolic sine. Domain=(-inf, inf)."""
     return x.double().add(torch.sqrt(1 + x.double().pow(2))).log().to(x.dtype)
 
-def artanh(x):
+def artanh(x: torch.Tensor) -> torch.Tensor:
     """Inverse hyperbolic tangent. Domain=(-1, 1)."""
     eps = torch.finfo(x.dtype).eps
     x = x.clamp(-1 + eps, 1 - eps)
