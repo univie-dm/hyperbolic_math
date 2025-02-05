076832991557537f1a6031447fcb588513bc69af
Kevin Sidak <kevin.sidak@univie.ac.at>
2025-02-05 14:09:38 +0000

    Update skip message for unimplemented tests in test_optimizers.py

--- tests/test_optimizers.py
+++ tests/test_optimizers.py
@@ -6,7 +6,8 @@ import torch
 from src.manifolds import Euclidean, Hyperboloid, ManifoldParameter, PoincareBall
 from src.optim import RiemannianAdam, RiemannianSGD
 
-@pytest.skip("Not implemented", allow_module_level=True)
+#TODO Not updated for new manifolds yet
+@pytest.skip("Not updated yet", allow_module_level=True)
 
 @pytest.fixture(scope="module")
 def tolerance(dtype: torch.dtype) -> Tuple[float, float]:
